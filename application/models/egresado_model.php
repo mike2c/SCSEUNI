011146601d35e7263403932691b66a92f3cc8248
mike2c <mikedosce1992@gmail.com>
2015-05-13 13:02:59 -0600

    asdsadas

--- application/models/egresado_model.php
+++ application/models/egresado_model.php
@@ -22,12 +22,27 @@
 		
 		}
 
-		function actualizarEgresado($data){
+		function actualizarEgresado($data_egresado,$data_persona,$data_usuario,$data_contacto){
+			
+			$this->db->where('persona_id',$data_persona["persona_id"]);
+			$this->db->update('persona',$data_persona);
+
+			$this->db->where('contacto_id',$data_contacto["contacto_id"]);
+			$this->db->update("contacto",$data_contacto);
+
+			$this->db->where("usuario_id",$data_usuario["usuario_id"]);
+			$this->db->update("usuario",$data_usuario);
+
+			$this->db->where("egresado_id",$data_egresado["egresado_id"]);
+			$this->db->update("egresado",$data_egresado);
 
 		}
 
-		function eliminarEgresado($data){
-
+		function eliminarEgresado($persona_id,$egresado_id,$contacto_id,$usuario_id){
+			$this->db->query("delete from usuario where usuario_id=$usuario_id");
+			$this->db->query("delete from contacto where contacto_id=$contacto_id");
+			$this->db->query("delete from egresado where egresado_id=$egresado_id");
+			$this->db->query("delete from persona where persona_id=$persona_id");
 		}
 	}
 
