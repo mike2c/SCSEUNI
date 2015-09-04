6379f0b4782c3529e248c8347932b00fe9c7013d
TheBlackR <reneblackr@gmail.com>
2015-09-04 12:53:39 -0600

    modal para crear ficha

--- application/views/ficha/crear_ficha - copia.php
+++ application/views/ficha/crear_ficha - copia.php
@@ -1,60 +1,63 @@
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#crearFicha">Crear Ficha</button>
-<div class="modal fade" id="crearFicha" role="dialog" aria-labelledby="crearFichaLabel">
-	<div class="modal-dialog" role="document">
-		<div class="modal-content" style="overflow-y:scroll; max-height:590px;">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="close"><span aria-hidden="true">&times;</span></button>
-				<h4 class="modal-title" id="crearFichaLabel">Nueva Ficha Ocupacional</h4>
-			</div>
+<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#crearFicha">
+<div class="modal fade" id="crearFicha" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+		<div style="padding:0px 10px;border-left:1px solid lightgray">
+			<h3 class="modal-title">Nueva ficha ocupacional</h3>
 			<div class="modal-body">
 				<form action="<?=base_url('Ficha/Crear')?>" name="formCrearFicha" id="formCrearFicha" method="post" enctype="multipart/form-data">
-					<div id="invalid">
+					<div class="form-group" id="invalid">
 						<?php echo validation_errors(); ?>
 					</div>
 					<div class="form-group">
-					<label for="descripcion">Breve Descripción para la publicación(<small>Obligatorio</small>):</label><br>
-					<textarea required name="descripcion"  id="descripcion" cols="30" rows="4" class="form-control" data-trigger="focus" data-container="body" data-toggle="popover" data-placement="top" title="Descripción de la publicación" data-content="
-					Escriba descripción acerca de la publicación, explicando brevemente la finalidad del cargo y sus funciones."><?php echo set_value("descripcion"); ?></textarea>
-					</div>
-
-					<div class="form-group">
-					<label class="">Elige una imagen para la publicación(<small>Opcional</small>):</label><br>
-					<input content="balbla" class="modal-body" type="file" name="imagen" id="imagen"/>
-					</div>
-
-					<div class="form-group" style="">
-						<label for="" class="">Denominación del cargo(<small>Obligatorio</small>):</label><br>	
-						<input required value="<?php echo set_value("cargo"); ?>" type="text" class="form-control" name="cargo" data-trigger="focus" data-toggle="popover" data-placement="top" title="Denominación del Cargo" data-content="
-						Escriba el nombre del cargo, ejemplo: (Gerente General, Administrador, Contador, etc.)">
+						<label class="">Elige una imagen para la publicación(<small>Opcional</small>)</label><br>
+						<img id="precarga" onclick="buscarImagen()" src="<?=base_url('public/res/add_image.png')?>" alt="La imagen no se puede visualizar">
+						<input type="file" name="imagen" id="imagen" >
 					</div>
 					<div class="form-group">
-						<label  for="" class="">Ubicación de cargo(<small>Obligatorio</small>):</label><br>		
-						<input required value="<?php echo set_value("ubicacion"); ?>" type="text" class="form-control" name="ubicacion" data-trigger="focus" data-toggle="popover" data-placement="top" title="Ubicacion del Cargo" data-content="
-						Escriba el área en la que se ubica el Cargo, (ejemplo: Gerencia, Finanzas, etc.)">
+						<label for="">Breve Descripción para la publicación(<small>Obligatorio</small>):</label><br>
+						<textarea required name="descripcion"  id="descripcion" cols="30" rows="4" class="form-control" data-trigger="focus" data-container="body" data-toggle="popover" data-placement="top" title="Descripción de la publicación" data-content="
+						Escriba descripción acerca de la publicación, explicando brevemente la finalidad del cargo y sus funciones."><?php echo set_value("descripcion"); ?></textarea>
 					</div>
-
+					<div class="form-inline" style="">
+						<div class="form-group" style="">
+							<label for="" class="">Denominación del cargo(<small>Obligatorio</small>)</label><br>	
+							<input required style="width:360px;" value="<?php echo set_value("cargo"); ?>" type="text" class="form-control" name="cargo" data-trigger="focus" data-toggle="popover" data-placement="top" title="Denominación del Cargo" data-content="
+							Escriba el nombre del cargo, ejemplo: (Gerente General, Administrador, Contador, etc.)">
+						</div>
+						<div class="form-group">
+							<label  for="" class="">Ubicación de cargo(<small>Obligatorio</small>)</label><br>		
+							<input required style="width:360px;" value="<?php echo set_value("ubicacion"); ?>" type="text" class="form-control" name="ubicacion" data-trigger="focus" data-toggle="popover" data-placement="top" title="Ubicacion del Cargo" data-content="
+							Escriba el área en la que se ubica el Cargo, (ejemplo: Gerencia, Finanzas, etc.)">
+						</div>
+					</div>
+					<br>
+					<div class="form-inline">
+						<div class="form-group">
+							<label for="" class="">Cantidad de puestos(<small>Obligatorio</small>)</label><br>	
+							<input required maxlength="4" value="<?php echo set_value("cantidad"); ?>" title="Digite un valor numerico" pattern="^([1-9])" style="width:360px;" type="text" class="form-control" name="cantidad" data-trigger="focus" data-toggle="popover" data-placement="top" title="cantidad de puestos" data-content="
+							Escriba la cantidad de puestos disponibles para dicho cargo.">
+						</div>
+							<div class="form-group">
+							<label for="" class="">Jefe inmediato(<small>Obligatorio</small>)</label><br>		
+							<input style="width:360px;" value="<?php echo set_value("jefe"); ?>" type="text" class="form-control" name="jefe" data-toggle="popover" data-placement="top" title="Jefe inmediato" data-trigger="focus" data-content="
+							Digite el cargo del jefe inmediato del puesto vacante, (ejemplo: Gerente, Administrador, etc.)">
+						</div>
+					</div><br>
 					<div class="form-group">
-						<label for="" class="">Cantidad de puestos(<small>Obligatorio</small>):</label><br>	
-						<input required maxlength="4" value="<?php echo set_value("cantidad"); ?>" title="Digite un valor numerico" pattern="^([1-9])" type="text" class="form-control" name="cantidad" data-trigger="focus" data-toggle="popover" data-placement="top" title="cantidad de puestos" data-content="
-						Escriba la cantidad de puestos disponibles para dicho cargo.">
-					</div>
-					<div class="form-group">:
-						<label for="" class="">Jefe inmediato(<small>Obligatorio</small>):</label><br>		
-						<input value="<?php echo set_value("jefe"); ?>" type="text" class="form-control" name="jefe" data-toggle="popover" data-placement="top" title="Jefe inmediato" data-trigger="focus" data-content="
-						Digite el cargo del jefe inmediato del puesto vacante, (ejemplo: Gerente, Administrador, etc.)">
-					</div>
-					<div class="form-group">
-					<label for="" class="">Experiencia(<small>Obligatorio</small>):</label>		
+					<label for="" class="">Experiencia(<small>Obligatorio</small>)</label>		
 						<input required style="" value="<?php echo set_value("experiencia"); ?>"  type="text" class="form-control" name="experiencia" data-toggle="popover" data-placement="top" title="Experiencia"  data-trigger="focus" data-content="
 						Ingrese la experiencia requerida para el cargo ofertado.">
 					</div>
 					<div class="form-group">
-						<label for="" class="">Personal a cargo(<small>Obligatorio</small>):</label>		
+						<label for="" class="">Personal a cargo(<small>Obligatorio</small>)</label>		
 						<textarea class="form-control" name="a_cargo" id="a_cargo" cols="30" rows="4" data-toggle="popover" data-placement="top" title="Personal a Cargo" data-trigger="focus" data-content="
 						Escriba los cargos del personal que estar bajo la responsabilidad del puesto ofertado. (Ejemplo: Administrador, Contador, etc.)
 						Nota =  en caso de que el puesto no tenga personal a cargo, escribir ninguno.
 						"><?php echo set_value("a_cargo"); ?></textarea>
 					</div>
+					
 					<div class="form-group">
 						<label for="" class="">Finalidad(<small>Obligatorio</small>)</label>		
 						<textarea class="form-control" name="finalidad" id="" cols="30" rows="5" data-toggle="popover" data-placement="top" title="Finalidad" data-trigger="focus" data-content="
@@ -65,6 +68,7 @@
 						<textarea class="form-control" name="requisitos" id="requisitos" cols="30" rows="5" data-toggle="popover" data-placement="top" title="Requisitos" data-trigger="focus" data-content="
 						Describa las destrezas o estudios necesarios para aplicar al cargo ofertado, (ejemplo: Ingeniería industrial, ingeniería de sistemas, etc.)"><?php echo set_value("requisitos"); ?></textarea>
 					</div>
+					
 					<div class="form-group">
 						<label for="" class="">Competencia(<small>Obligatorio</small>)</label>		
 						<textarea class="form-control" name="competencia" id="" cols="30" rows="5" class="form-control" data-toggle="popover" data-placement="top" title="Competencia" data-trigger="focus" data-content="
@@ -101,15 +105,26 @@
 							}
 						?>
 					</div>
-				</form>
 			</div>
 			<div class="modal-footer">
-				<button form="formCrearFicha" class="btn btn-primary" type="submit">Guardar</button>
-				<button form="formCrearFicha" type="reset" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+					<div class="">
+						<button class="btn btn-primary" type="submit">Guardar</button>
+						<button type="reset" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+					</div>
+					<div class="form-group">
+						<p id="shown_errors">
+							
+						</p>
+					</div>
+						<br>
+				</form>
 			</div>
-		</div>	
+			</div>
+		</div>
 	</div>
 </div>
+</div>
+
 <style type="text/css">
 	form img{
 		border:1px solid lightgray;
@@ -123,6 +138,9 @@
 	}
 	img:hover{
 		border: 2px solid gray;
+	}
+	input[type="file"]{
+		display: none;
 	}
 	#invalid{color: red;}
 </style>
