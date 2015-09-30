5b52d861d173d124e1bc60c02986bfe87436b839
Miguel Angel Castillo Cornejo <mikedosce1992@gmail.com>
2015-09-30 17:19:26 -0600

    Removiendo welcome_message

--- application/controllers/Empresa.php
+++ application/controllers/Empresa.php
@@ -3,65 +3,16 @@
 	
 	class Empresa extends CI_Controller{
 
+		/*Declaracion global de la variable empresa de tipo Empresa_modelo*/
 		function __construct(){
-
 			parent::__construct();
-			$this->load->library(array('session'));
-			$this->load->helper(array('form','url'));	
-	
+		
+			$this->load->model("empresa_model","empresa");
 		}
 
-		function Actualizar(){
-			$this->load->model("empresa_model","modelo");
-			$this->load->model("listas_model","lista");
-			$this->load->library(array("form_validation",'MY_validation'));
+		private function validarCampos(){
 
-			
-			$this->form_validation->set_rules("nombre_empresa","Nombre de la empresa","required|trim|max_length[45]");
-			$this->form_validation->set_rules("correo","Correo","required|trim|max_length[45]");
-			$this->form_validation->set_rules("nombre_empresa","Nombre","required|trim|max_length[45]");
-			
-			$this->form_validation->set_rules("municipio","Municipio","required");
-			$this->form_validation->set_rules("direccion","Dirección","max_length[100]");
-			$this->form_validation->set_rules("telefono","Telefono","trim|max_length[10]");
-
-			if($this->form_validation->run() == false){
-				echo validation_errors();
-			}else{
-			
-				$data["empresa"]["empresa_id"] = $this->input->post("empresa_id");
-				$data["empresa"]["mision"] = $this->input->post("mision");
-				$data["empresa"]["vision"] = $this->input->post("vision");
-				$data["empresa"]["ocupacion"] = $this->input->post("ocupacion");
-				$data["empresa"]["nombre_empresa"] = $this->input->post("nombre_empresa");
-				$data["empresa"]["ruc"] = $this->input->post("ruc");
-				$data["empresa"]["sociedad_id"] = $this->input->post("sociedad");
-				$data["empresa"]["sitio_web"] = $this->input->post("sitio_web");
-
-				$data["contacto"]["contacto_id"] = $this->input->post("contacto_id");
-				$data["contacto"]["telefono"] = $this->input->post("telefono");
-				$data["contacto"]["celular"] = $this->input->post("celular");
-				$data["contacto"]["direccion"] = $this->input->post("direccion");
-				$data["contacto"]["municipio_id"] = $this->input->post("municipio");
-
-				$data["usuario"]["usuario_id"] = $this->input->post("usuario_id");
-				$data["usuario"]["correo"] = $this->input->post("correo");
-				
-				$this->modelo->actualizarEmpresa($data["empresa"],$data["contacto"],$data["usuario"]);
-				
-			}
-			
-		}
-
-		function ActualizarPerfil(){
-			$this->Actualizar();
-			redirect("Perfil");
-		}
-
-		function Registro(){
 			$this->load->library("form_validation");
-			$this->load->library("Encrypter");
-
 			$this->form_validation->set_rules('nombre_empresa','Nombre de la Empresa','trim|required|min_length[2]|max_length[60]');
 			$this->form_validation->set_rules('telefono','Telefono','trim|max_length[10]|min_length[8]');
 			$this->form_validation->set_rules('direccion','Direccion','trim|max_length[100]');
@@ -70,134 +21,95 @@
 			$this->form_validation->set_rules('clave_conf','Contraseña','trim|required|matches[clave]|min_length[6]|max_length[100]',
 				array("matches"=>"Las contraseñas no coinciden","required"=>"El campo de confirmacion de contraseña es obligatorio"));
 			
+			if($this->form_validation->run() == FALSE){
+				echo validation_errors();
+				return FALSE;
+			}
 
-			$this->load->model('empresa_model','',TRUE);
+			return TRUE;
+		}
 
-			if($this->form_validation->run()==FALSE){
+		function Actualizar(){
 
-				if(IS_AJAX){
-					echo validation_errors();
-				}else{
-					$this->load->model("listas_model","departamento");
-					$data["departamentos"] = $this->departamento->listarDepartamentos();
-					$this->load->view("cabecera");
-					$this->load->view("nav");
-					$this->load->view("registro_empresa",$data);
-					$this->load->view("footer");
-				}
-		
+			if(IS_AJAX){
+
+				$this->validarCampos();				
 			}else{
-				$data_empresa['nombre_empresa'] = $this->input->post('nombre_empresa');
-
-				$data_usuario['correo'] = $this->input->post('correo');
-				$data_usuario['clave'] = Encrypter::encrypt($this->input->post('clave'));
-				$data_usuario['activo'] = TRUE;
-			
-				$data_contacto['telefono'] = $this->input->post('telefono');
-				$data_contacto['direccion'] = $this->input->post('direccion');
-				$data_contacto['municipio_id'] = $this->input->post('municipio');
-
-				$this->empresa_model->insertarEmpresa($data_usuario,$data_empresa,$data_contacto);
-			}
-			
-		} 
-
-		function Eliminar(){
-
-		}
-
-		function subirImg(){
-			$config['upload_path'] = './uploads/';
-			$config['allowed_types'] = 'gif|jpeg|png|jpg';
-			$config['max_size'] = '2048';
-			$config['max_width'] = '400';
-			$config['max_height'] = '400';
-
-			$this->load->library('upload',$config);
-
-			if(! $this->upload->do_upload()){
-				$error = array('error'=>$this->upload->display_errors());
-				$this->load->view('update_empresa',$error);
-			}else {
-				$data = array('upload_data'=>$this->upload->data());
-				return $data['upload_data']['file_name'];
-			}
-		}
-	}
-
-	/*		function _actualizar(){
-			$this->load->model('empresa_model','',TRUE);
-			$info["sociedad"] = $this->empresa_model->getInfo();
-			$info["empresa"] = $this->empresa_model->fillCampos();
-			$user_id = $this->session->userdata('empresa');
-
-			$this->load->view("cabecera"); 
-			$this->load->view("nav");
-			$this->load->view("update_empresa",$info);
-			$this->load->view("footer");
-
-			if ($this->input->post('nombre_empresa') and $this->input->post('nombre_empresa') !="") {
-				$this->form_validation->set_rules('nombre_empresa','Nombre de la Empresa','trim|required|min_length[2]|max_length[45]');			
-				$this->form_validation->set_rules('sitio_web','Sitio Web','trim|max_length[100]');
-				$this->form_validation->set_rules('ocupacion','Ocupacion','trim|max_length[500]');
-				$this->form_validation->set_rules('mision','Mision','trim|max_length[500]');
-				$this->form_validation->set_rules('vision','Vision','trim|max_length[500]');
-				$this->form_validation->set_rules('sociedad','Sociedad','trim|max_length[40]');
-
-				$data_empresa['empresa_id'] = $info["empresa"]["data_empresa"]->row()->empresa_id;
-				$data_empresa['nombre_empresa'] = $this->input->post('nombre_empresa');
-				$data_empresa['sociedad_id'] = $this->input->post('sociedad');
-				$data_empresa['mision'] = $this->input->post('mision');
-				$data_empresa['vision'] = $this->input->post('vision');
-				$data_empresa['ocupacion'] = $this->input->post('ocupacion');
-				$data_empresa['sitio_web'] = $this->input->post('sitio_web');
-
-				if($this->form_validation->run() == TRUE){
-				$msg = $this->empresa_model->editarEmpresa($data_empresa);
-					if($msg){
-
-					}
-				}
-			}else{}
-				if ($this->input->post('correo') !=="" and $this->input->post('correo')) {
-				$this->form_validation->set_rules('correo','Correo','trim|required|max_length[45]|valid_email');
-				$this->form_validation->set_rules('clave','Contraseña','trim|required|min_length[6]|max_length[100]');
-
-				$data_usuario['usuario_id']  = $user_id['usuario_id'];
-				$img = $this->subirImg();
-					if($img == ""){
-
-					}else{
-						$data_usuario['imagen_perfil'] = $this->subirImg();
-					}
 				
-				$data_usuario['correo'] = $this->input->post('correo');
-				$data_usuario['clave'] = $this->input->post('clave');
+				if($this->validarCampos()){
 
-				if($this->form_validation->run() == TRUE){
-				$msg = $this->empresa_model->editarUsuarioEmpresa($data_usuario);
-					if($msg){
+					#Tabla empresa
+					$data["empresa"]["empresa_id"] = $this->input->post("empresa_id");
+					$data["empresa"]["mision"] = $this->input->post("mision");
+					$data["empresa"]["vision"] = $this->input->post("vision");
+					$data["empresa"]["ocupacion"] = $this->input->post("ocupacion");
+					$data["empresa"]["nombre_empresa"] = $this->input->post("nombre_empresa");
+					$data["empresa"]["ruc"] = $this->input->post("ruc");
+					$data["empresa"]["sociedad_id"] = $this->input->post("sociedad");
+					$data["empresa"]["sitio_web"] = $this->input->post("sitio_web");
 
-					}
-				}
-			}else{}
-				if ($this->input->post('telefono')!=="" and $this->input->post('telefono')) {
-				#FAlTA MUNCIPIO
-					$this->form_validation->set_rules('direccion','Direccion','trim|max_length[100]');
-					$this->form_validation->set_rules('telefono','Telefono','trim|max_length[10]|min_length[8]');
-					$this->form_validation->set_rules('celular','Celular','trim|max_length[10]|min_length[8]');
+					#Tabla contacto
+					$data["contacto"]["contacto_id"] = $this->input->post("contacto_id");
+					$data["contacto"]["telefono"] = $this->input->post("telefono");
+					$data["contacto"]["celular"] = $this->input->post("celular");
+					$data["contacto"]["direccion"] = $this->input->post("direccion");
+					$data["contacto"]["municipio_id"] = $this->input->post("municipio");
 
-					$data_contacto['contacto_id'] = $info["empresa"]["data_empresa"]->row()->contacto_id;
+					#Tabla usuario
+					$data["usuario"]["usuario_id"] = $this->input->post("usuario_id");
+					$data["usuario"]["correo"] = $this->input->post("correo");
+					
+					$this->empresa->actualizarEmpresa($data["empresa"],$data["contacto"],$data["usuario"]);
+				
+				}//END IF
+
+			}//END ELSE
+			
+		}//END FUNCTION
+
+
+		/*Actualizar perfil de empresa*/
+		function ActualizarPerfil(){
+
+			if($this->Actualizar()){
+				echo "<script type='text/javascript'>
+					alert('Perfil actualizado');
+					window.location='". base_url('Perfil') ."';
+				</script>";
+			}
+		}
+
+		/*Registrar una nueva empresa*/
+		function Registro(){
+			
+			if(IS_AJAX){
+
+				$this->validarCampos();
+			}else{
+
+				if($this->validarCampos()){
+
+					$this->load->library("Encrypter");
+
+					#Tabla empresa
+					$data_empresa['nombre_empresa'] = $this->input->post('nombre_empresa');
+					
+					#Tabla usuario
+					$data_usuario['correo'] = $this->input->post('correo');
+					$data_usuario['clave'] = Encrypter::encrypt($this->input->post('clave'));
+					$data_usuario['activo'] = TRUE;
+					
+					#Tabla contacto
 					$data_contacto['telefono'] = $this->input->post('telefono');
-					$data_contacto['celular'] = $this->input->post('celular');
 					$data_contacto['direccion'] = $this->input->post('direccion');
+					$data_contacto['municipio_id'] = $this->input->post('municipio');
 
-					if($this->form_validation->run() == TRUE){
-					$msg = $this->empresa_model->editarContactoEmpresa($data_contacto);
-						if($msg){
-		
-						}
-					}
-				}
-			}*/
+					$this->empresa->insertarEmpresa($data_usuario,$data_empresa,$data_contacto);
+					
+				}/*END IF*/
+
+			}/*END ELSE*>
+
+		}/*END FUNCTION*/
+	}
 ?>
--- application/controllers/Publicador.php
+++ application/controllers/Publicador.php
@@ -5,158 +5,100 @@
 
 		function __construct(){
 			parent::__construct();
-		}
-
-		function Index(){
-			$this->load->view("cabecera");
-			$this->load->view("registro_publicador");
+			$this->load->model("publicador_model","publicador");
 		}
 
 		function Registro(){
 
-			$this->load->library("form_validation");
-			
-			$rules = array();
-			array_push($rules,array("field"=>"nombre","label"=>"Nombre","rules"=>"required"));
-			$this->form_validation->set_rules($rules);
-			/*AQUIIII*/
-			/*Terminar de valiadar*/
-
 			if(!IS_AJAX){
-				if(!$this->validarCampos()){
-					echo validation_errors();
-					$this->load->view("cabecera");
-					$this->load->view("nav");
-					$this->load->view("publicador/registro_publicador");
-					$this->load->view("footer");
-				}else{
-					$this->Registrar();
-				}
+				$this->validarCampos();
 			}else{
-				if(!$_POST){
-					$this->load->view("publicador/registro_publicador");
-				}else{
-					if(!$this->validarCampos()){
-						echo validation_errors();
+
+				/*Volvemos a preguntar si los campos recibidos son validos para cuando
+				se haga una peticion sin previa validacion por AJAX*/
+				if($this->validarCampos()){
+					
+					$this->load->library("Encrypter");
+					$this->load->helper("fecha");
+
+					$data_persona["nombre"] = $this->input->post("nombre");
+					$data_persona["apellido"] = $this->input->post("apellido");
+					$data_persona["sexo"] = $this->input->post("genero");
+					$data_persona["fecha_nacimiento"] = format_date($this->input->post("fecha_nacimiento"));
+					
+					if($data_persona["sexo"] == "M"){
+						$data_usuario["imagen"]="male.jpeg";
 					}else{
-						$this->Registrar();
+						$data_usuario["imagen"]="female.jpeg";
 					}
-				}
-			}
-		
-		}
 
-		private function Registrar(){
-			//LOADS
-			$this->load->library("Encrypter");
-			$this->load->model("Publicador_model","publicador");
+					$data_usuario["correo"] = $this->input->post("correo");
+					$data_usuario["clave"] = Encrypter::encrypt($this->input->post("clave"));
+					$data_usuario["activo"] = TRUE;
 
-			//CAPTURANDO LOS DATOS
-			$data_persona["nombre"] = $this->input->post("nombre");
-			$data_persona["apellido"] = $this->input->post("apellido");
-			$data_persona["sexo"] = $this->input->post("genero");
-			
-			$originalDate = str_replace("/", "-",$this->input->post("fecha_nacimiento") );
-			$newDate = date("Y-m-d", strtotime($originalDate));
-			$data_persona["fecha_nacimiento"] = $newDate;
-			
-			if($data_persona["sexo"] == "M"){
-				$data_usuario["imagen"]="male.jpeg";
-			}else{
-				$data_usuario["imagen"]="female.jpeg";
-			}
+					$data_publicador["cargo_id"] = $this->input->post("cargo");
 
-			$data_usuario["correo"] = $this->input->post("correo");
-			$data_usuario["clave"] = Encrypter::encrypt($this->input->post("clave"));
-			$data_usuario["activo"] = TRUE;
-
-			$data_publicador["cargo_id"] = $this->input->post("cargo");
-
-			$this->publicador->insertar($data_persona,$data_usuario,$data_publicador);
-		
-		}
-
-		function Listar(){
-
-			$this->load->model("publicador_model","modelo");
-			$data["publicadores"] = $this->modelo->listar();
-			
-			if(IS_AJAX){
-				$this->load->view("publicador/busqueda_publicador",$data);
-				$this->load->view("publicador/listar_publicadores",$data);
-			}else{
-				$this->load->view("cabecera",$data);
-				$this->load->view("nav",$data);
-				$this->load->view("publicador/busqueda_publicador",$data);
-				$this->load->view("publicador/listar_publicadores",$data);
-				$this->load->view("footer",$data);
-			}
-		}
-
-		function Busqueda(){
-
-			if($_POST){
-				$this->load->model("publicador_model","modelo");
-				$data["publicadores"] = $this->modelo->buscarPublicador($_POST);
-
-				if(IS_AJAX){
-					$this->load->view("publicador/listar_publicadores",$data);
-				}else{
-					$this->load->view("publicador/busqueda_publicador");
-					$this->load->view("publicador/listar_publicadores",$data);
+					$this->publicador->insertar($data_persona,$data_usuario,$data_publicador);
 				}
 			}
 		}
 
 		function ActualizarPerfil(){
 
-			$this->Actualizar();
-			redirect("Perfil");
+			if($this->Actualizar()){
+				echo "<script type='text/javascript'>
+					alert('Perfil actualizado');
+					window.location='". base_url('Perfil') ."';
+				</script>";
+			}
+		
 		}
 
 		function Actualizar(){
 
-			$this->load->library("Encrypter");
-			$this->load->model("Publicador_model","publicador");
+			if(IS_AJAX){
 
-			$data_persona["persona_id"] = $this->input->post("persona_id");
-			$data_persona["nombre"] = $this->input->post("nombre");
-			$data_persona["apellido"] = $this->input->post("apellido");
-			$data_persona["sexo"] = $this->input->post("genero");
-			
-			$originalDate = str_replace("/", "-",$this->input->post("fecha_nacimiento") );
-			$newDate = date("Y-m-d", strtotime($originalDate));
-			$data_persona["fecha_nacimiento"] = $newDate;
-			
-			$data_usuario["usuario_id"] = $this->input->post("usuario_id");
-			$data_usuario["correo"] = $this->input->post("correo");
+				$this->validarCampos();
+			}else{
 
-			$data_publicador["publicador_id"] = $this->input->post("publicador_id");
-			$data_publicador["cargo_id"] = $this->input->post("cargo");
+				if($this->validarCampos()){
+
+					$this->load->library("Encrypter");
+					$this->load->helper("fecha");
+
+					$data_persona["persona_id"] = $this->input->post("persona_id");
+					$data_persona["nombre"] = $this->input->post("nombre");
+					$data_persona["apellido"] = $this->input->post("apellido");
+					$data_persona["sexo"] = $this->input->post("genero");
+					
+					$data_persona["fecha_nacimiento"] = format_date($this->input->post("fecha_nacimiento"));
+					
+					$data_usuario["usuario_id"] = $this->input->post("usuario_id");
+					$data_usuario["correo"] = $this->input->post("correo");
+
+					$data_publicador["publicador_id"] = $this->input->post("publicador_id");
+					$data_publicador["cargo_id"] = $this->input->post("cargo");
 			
-			if($data_publicador["cargo_id"] == ""){
-				return;
+					$this->publicador->actualizar($data_usuario,$data_persona,$data_publicador);
+				}
+			}
+		}
+
+		function validarCampos(){
+
+			$this->load->library("form_validation");
+			$this->form_validation->set_rules("nombre","Nombre","trim|required|max_length[45]");
+			$this->form_validation->set_rules("apellido","Apellido","trim|required|max_length[45]");
+			$this->form_validation->set_rules("nombre","Genero","trim|required|min_length[1]");
+			$this->form_validation->set_rules("cargo","Cargo","trim|required|min_length[1]");
+			
+			if($this->form_validation->run()==false){
+				
+				echo validation_errors();
+				return false;
 			}
 
-			$this->publicador->actualizar($data_usuario,$data_persona,$data_publicador);
-
-		}
-
-		function Eliminar($id_publicador){
-
-		}
-		
-		function validarCampos(){
-				$this->form_validation->set_rules("nombre","Nombre","trim|required|max_lenght[45]");
-				$this->form_validation->set_rules("apellido","Apellido","trim|required|max_lenght[45]");
-				$this->form_validation->set_rules("nombre","Genero","trim|required|max_lenght[1]");
-				$this->form_validation->set_rules("cargo_id","Cargo","trim|required|max_lenght[1]");
-				
-				if($this->form_validation->run()==false){
-					return FALSE;
-				}else{
-					return TRUE;
-				}
+			return true;
 		}
 	}
 
