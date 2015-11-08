606bfd209146f60e15fda9792145f3a5c83fa686
Miguel Angel Castillo Cornejo <mikedosce1992@gmail.com>
2015-11-07 18:22:01 -0600

    Cambios en la pagina de home

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -7,38 +7,42 @@
 	</ul>
 </div>
 <div class="container">
-	<div class="row">
-		<div class="col-md-4 no-padding">
-			<article>
-				<h4 class="text-primary"><strong>Objetivos</strong></h4>
-				<p class="text-justify">
-					<small>Impulsar vínculos permanentes de apoyo y beneficio mutuo entre el Recinto Universitario Augusto C. Sandino, sede regional del norte con sus egresados y graduados, así como crear y mantener relaciones entre la universidad y los empleadores de la Región Norte del país facilitando la inserción laboral de los egresados y graduados del Recinto.</small>					
-				</p>
-			</article>
-		</div>
-		
-		<div class="col-md-4">	
-			<article>
-				<h4 class="text-primary"><strong>Misión</strong></h4>	
-				<small><p class="text-justify">El programa de seguimiento a graduados del Recinto Universitario Augusto C. Sandino, sede regional del norte es la instancia institucional responsable de la vinculación dinámica y permanente entre los egresados, graduados, la universidad y empleadores, para facilitar la mejor y actualización del currículo de las carreras, el vínculo con el campo laboral y la formación permanente.</p></small>
-			</article>
-		</div>
-		
-		<div class="col-md-4">
-			<article>
-				<h4 class="text-primary"><strong>Visión</strong></h4>
-				<p class="text-justify">
-					<small>Ser un programa competente para fortalecer la pertinencia de la formación profesional, a través de la interacción institucional del Recinto Universitario Augusto C. Sandino, sede regional del norte con los graduados y los empleadores que posibilitan su inserción laboral en cada una de las actividades dirigidas  a la trasformación de la sociedad y su entorno.</small>					
-				</p>
-			</article>
-		</div>
-	</div>	
-</div>
-<div style="background-color: white;">
-	<div class="container" style="margin-top:10px;padding-top:10px;">
-		<div class="row" style="">
+	<div class="contenido">
+		<div class="row">
+			<div class="col-md-4 no-padding">
+				<article>
+					<h4 class="text-primary"><strong>Objetivos</strong></h4>
+					<p class="text-justify">
+					Impulsar vínculos permanentes de apoyo y beneficio mutuo entre el Recinto Universitario Augusto C. Sandino, sede regional del norte con sus egresados y graduados, así como crear y mantener relaciones entre la universidad y los empleadores de la Región Norte del país facilitando la inserción laboral de los egresados y graduados del Recinto.					
+					</p>
+				</article>
+			</div>
+			
+			<div class="col-md-4">	
+				<article>
+					<h4 class="text-primary"><strong>Misión</strong></h4>	
+				<p class="text-justify border-left">El programa de seguimiento a graduados del Recinto Universitario Augusto C. Sandino, sede regional del norte es la instancia institucional responsable de la vinculación dinámica y permanente entre los egresados, graduados, la universidad y empleadores, para facilitar la mejor y actualización del currículo de las carreras, el vínculo con el campo laboral y la formación permanente.</p>
+				</article>
+			</div>
+			
 			<div class="col-md-4">
-				<h4 class="box-title text-primary">Contacto</h4>
+				<article>
+					<h4 class="text-primary"><strong>Visión</strong></h4>
+					<p class="text-justify border-left">
+					Ser un programa competente para fortalecer la pertinencia de la formación profesional, a través de la interacción institucional del Recinto Universitario Augusto C. Sandino, sede regional del norte con los graduados y los empleadores que posibilitan su inserción laboral en cada una de las actividades dirigidas  a la trasformación de la sociedad y su entorno.					
+					</p>
+				</article>
+			</div>	
+		</div>	
+	</div>
+</div>
+<br>
+
+<div style="background-color: white;margin-top:10px;padding-top:20px;border-top:1px solid lightgray;">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<h3 class="text-left text-primary">Contacto</h3>
 				<h5 class="text-info">Dirección</h5>
 					<p class="help-block">
 						Entrada a La Tunoza, Antigua Hacienda el Higo, Estelí, Nicaragua
@@ -53,14 +57,14 @@
 					</p>
 			</div>
 			<div class="col-md-4">
-				<h4 class="box-title text-primary">Sitios de interés</h4>
+				<h3 class="text-left text-primary">Sitios de interés</h3>
 				<ul>
-					<li><a href="http://www.uni.edu.ni/" target="_new">Universidad Nacional de Ingeniería</a></li>
-					<li><a href="http://norte.uni.edu.ni/" target="_new">Universidad Nacional de Ingeniería, Recinto Universitario Augusto C. Sandino</a></li>
+					<li><a href="http://www.uni.edu.ni/" target="_new"><span class="glyphicon glyphicon-triangle-right"></span> Universidad Nacional de Ingeniería</a></li>
+					<li><a href="http://norte.uni.edu.ni/" target="_new"><span class="glyphicon glyphicon-triangle-right"></span> Universidad Nacional de Ingeniería, Recinto Universitario Augusto C. Sandino</a></li>
 				</ul>
 				<h5 class="text-info">Síguenos en</h5>
 				<ul>
-					<li><a href="https://www.facebook.com/norte.uni.edu.ni?fref=ts" target="_new">Facebook - Uni Ruacs sede Estelí</a></li>
+					<li><a href="https://www.facebook.com/norte.uni.edu.ni?fref=ts" target="_new"><span class="glyphicon glyphicon-triangle-right"></span> Facebook - Uni Ruacs sede Estelí</a></li>
 				</ul>
 			</div>
 		</div>
@@ -85,13 +89,16 @@
 </script>
 <style type="text/css">
 	ul{
-		list-style: square;
+		list-style: none;
 	}
+
 	.jumbotron{
 		background-color: #03447F;
 	}
 	.pgwSlider{
-		height : 150px
+		margin-top:10px;
+		height : 150px;
+		margin-bottom: 10px;
 	}
 
 	header{
@@ -107,17 +114,17 @@
 	}
 	
 	article{
-
-		border: 1px solid #aeaeae;
-		border-left: 5px solid #337ab7;
-		padding: 10px 20px;
-		background-color: white;
-		min-height: 220px;
-	}	
-	article h4{
-
-		margin-top: 10px;
-		margin-bottom: 10px;
+		//border-left: 2px solid lightgray;
+		padding: 10px 0px;
+		padding-bottom: 20px;
+		margin-top:10px;
+	}
+	article p,h4{
+		padding-left: 20px;
+		text-align: center;
+	}
+	.border-left{
+		border-left: 1px solid lightgray;
 	}
 	#banner{
 		margin-bottom: 10px;
